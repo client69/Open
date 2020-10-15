3363ee6e053fdbdec8c871264ecd20da4554e120
ayush_manglani <61349816+Ayushmanglani@users.noreply.github.com>
2020-10-15 12:41:25 +0530

    Create KadaneAlgorithm.py

---
+++ KadaneAlgorithm.py
@@ -0,0 +1,18 @@
+# Kadane's algorithm
+# Used to find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum.
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    a = list(map(int, input().split()))
+    maxglobal = -9999999999999999999
+    max_here = 0
+
+    for i in range(0, n): 
+        max_here = max_here + a[i] 
+        if (maxglobal < max_here): 
+            maxglobal = max_here 
+
+        if max_here < 0: 
+            max_here = 0   
+    print(maxglobal)
