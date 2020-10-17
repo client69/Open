0698f91bf4059b1d4c49e6e0af4cafed81e0642c
hahaharshita <66302968+hahaharshita@users.noreply.github.com>
2020-10-17 21:22:13 +0530

    Update cycle_detect.cpp

--- cycle_detect.cpp
+++ cycle_detect.cpp
@@ -146,3 +146,57 @@ Graph doesn't contain a cycle.
 
 
 */
+//dfs cycle detection 
+//directedd graph 
+#include<bits/stdc++.h>
+using namespace std;
+void make(vector<int>v[],int x,int y){
+   v[x].push_back(y);
+
+}
+bool cycle(vector<int>v[],int i,bool visted[],bool stk[]){
+   if(visted[i]==false){
+       visted[i]=true;
+       stk[i]=true;
+       for(auto x: v[i]){
+           if(visted[x]==false && cycle(v,x,visted,stk)){
+               return true;
+           }
+           else if(visted[x]==true && stk[x]==true){
+               return true;
+           }
+       }
+   }
+   stk[i]=false;
+    return false;
+}
+bool iscyclic(vector<int>v[],int n){
+   bool visted[n]={false};
+   bool stk[n]={false};
+   for(int i=0;i<n;i++){
+       if(cycle(v,i,visted,stk)==true){
+           return true;
+       }
+       
+   }
+   return false;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v[n];
+    int edges;
+    cin>>edges;
+    while(edges--){
+        int x,y;
+        cin>>x>>y;
+        make(v,x,y);
+    }
+    if(iscyclic(v,n)){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+    
+}
