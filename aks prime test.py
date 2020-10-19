b5e3e46161198e94c29beb550a2d3b5646b2292f
WANGCHEN722 <52946865+WANGCHEN722@users.noreply.github.com>
2020-10-19 20:49:55 +0800

    Create aks prime test.py

---
+++ aks prime test.py
@@ -0,0 +1,16 @@
+#AKS prime test is the fastest deterministic prime test as of now, it executes in polynomial time, useful for big primes
+
+def factorial(x):
+    p=1
+    for i in range(2,x+1):
+        p*=i
+    return p
+
+def isprime(n):
+    for i in range(n+1):
+        if n-i<i:
+            break
+        p=factorial(n)//(factorial(i)*factorial(n-i))
+        if p%n!=0 and p!=1:
+            return False
+    return True
