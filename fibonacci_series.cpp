035145ee247e98f5cb1e8f7fddaa0bbb4d6e08a7
hahaharshita <66302968+hahaharshita@users.noreply.github.com>
2020-10-19 10:01:43 +0530

    Update fibonacci_series.cpp

--- fibonacci_series.cpp
+++ fibonacci_series.cpp
@@ -60,3 +60,38 @@ in findFib(in n){
     return (((ans_matrix.a %mod) + (ans_matrix.b%mod))%mod);
     
 }
+// the above program is too complicated to understand try this one 
+//my cpp program
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, t1 = 0, t2 = 1, next = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i)
+    {
+        // Prints the first two terms.
+        if(i == 1)
+        {
+            cout << " " << t1;
+            continue;
+        }
+        if(i == 2)
+        {
+            cout << t2 << " ";
+            continue;
+        }
+        next = t1 + t2;
+        t1 = t2;
+        t2 = next;
+        
+        cout << next << " ";
+    }
+    return 0;
+}
