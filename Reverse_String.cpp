d85ce827bf439d6e6fe402c11d5f978cb73221ed
Ragubans <56040758+Ragubans@users.noreply.github.com>
2020-10-25 16:50:54 +0530

    Reverse_String.cpp

---
+++ Reverse_String.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+
+void reverseWords(string str) 
+{ 
+    stack<char> st; 
+  
+    for (int i = 0; i < str.length(); ++i) { 
+        if (str[i] != ' ') 
+            st.push(str[i]); 
+  
+       
+        else { 
+            while (st.empty() == false) { 
+                cout << st.top(); 
+                st.pop(); 
+            } 
+            cout << " "; 
+        } 
+    } 
+  
+    
+    while (st.empty() == false) { 
+        cout << st.top(); 
+        st.pop(); 
+    } 
+} 
