6bc2e31b67d23a060056e859e6b83a39834c0b23
krupanagar <73635830+krupanagar@users.noreply.github.com>
2020-10-30 16:47:49 +0530

    Update aes.py

--- aes.py
+++ aes.py
@@ -1,16 +1,3 @@
-#!/usr/bin/python
-#
-# aes.py: implements AES - Advanced Encryption Standard
-# from the SlowAES project, http://code.google.com/p/slowaes/
-#
-# Copyright (c) 2008    Josh Davis ( http://www.josh-davis.org ),
-#           Alex Martelli ( http://www.aleax.it )
-#
-# Ported from C code written by Laurent Haan ( http://www.progressive-coding.com )
-#
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/
-#
 import os
 import sys
 import math
